4d4f059db1747b105fc18b13c649a1feec2e62b2
Tamas K. Papp <tkpapp@gmail.com>
2017-03-31 09:07:50 +0200

    Moved test script to a separate file.
    Temporary workaround for https://github.com/JuliaPlots/Plots.jl/issues/753.

---
+++ test_script.jl
@@ -0,0 +1,16 @@
+## Julia test script to run on Travis
+## workaround until Pkg3 can REQUIRE master versions of packages
+
+## for these packages, use master
+Pkg.clone("https://github.com/tpapp/ContinuousTransformations.jl.git");
+## workaround for https://github.com/JuliaPlots/Plots.jl/issues/753
+if VERSION ≥ v"0.6-"
+    for pkg in ["Plots", "PlotThemes", "PlotUtils"]
+        Pkg.add(pkg)
+        Pkg.checkout(pkg)
+    end
+end
+
+## original test script
+Pkg.clone(pwd()); Pkg.build("ParametricFunctions");
+Pkg.test("ParametricFunctions"; coverage=true)
